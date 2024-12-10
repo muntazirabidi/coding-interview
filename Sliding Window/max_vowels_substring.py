5025a958356aa9885c388c73a817c011dfcb7b83
Muntazir Abidi <smuntazirabidi@gmail.com>
2024-12-10 23:02:09 +0000

    added a question on Sliding Window

---
+++ Sliding Window/max_vowels_substring.py
@@ -0,0 +1,40 @@
+'''
+Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+
+Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+
+ 
+
+Example 1:
+
+Input: s = "abciiidef", k = 3
+Output: 3
+Explanation: The substring "iii" contains 3 vowel letters.
+Example 2:
+
+Input: s = "aeiou", k = 2
+Output: 2
+Explanation: Any substring of length 2 contains 2 vowels.
+Example 3:
+
+Input: s = "leetcode", k = 3
+Output: 2
+Explanation: "lee", "eet" and "ode" contain 2 vowels.
+'''
+
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        vowels = {'a', 'e', 'i', 'o', 'u'}
+        
+        l, count, result = 0, 0, 0
+
+        for r in range(len(s)):
+            count += 1 if s[r] in vowels else 0
+
+            if  r - l + 1 > k:
+                count -= 1 if s[l] in vowels else 0
+                l += 1
+            
+            result = max(count, result)
+
+        return result
